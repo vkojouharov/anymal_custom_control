aa9e6d164fa0afe574fcb0c0836c80e9cc63a34e
StanleyWang1 <swang11@stanford.edu>
2026-03-11 17:41:15 -0700

    retune II

--- catkin_ws/src/anymal_custom_control/scripts/AUTO_anymal_arm_traj.py
+++ catkin_ws/src/anymal_custom_control/scripts/AUTO_anymal_arm_traj.py
@@ -75,9 +75,9 @@ ANYMAL_WAYPOINTS = [
 
 # Arm EE waypoints: (x, y, z) target positions in arm base frame [meters]
 ARM_WAYPOINTS = [
-    (1.05, -0.3, 0.456),     # waypoint 1
-    (0.4, 0.0, 0.0),     # waypoint 2
-    (0.4, 0.0, 0.0),     # waypoint 3
+    (1.05, -0.28, 0.5),     # waypoint 1
+    (0.4, 0.0, 0.2),     # waypoint 2
+    (0.4, 0.0, 0.2),     # waypoint 3
 ]
 
 # Boom stow position after each waypoint (joint-space, motor rad)
